a0b92abb5cd3e88cc2bb38eb58fab872f4b1551a
Ha-Mim <hamim.hyder@gmail.com>
2016-06-12 13:14:22 +0600

    Amount in words

---
+++ AmountInWords/ShowAmountInWords.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ShowAmountInWords
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.calculateButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.amountTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'calculateButton
+        '
+        Me.calculateButton.Location = New System.Drawing.Point(181, 88)
+        Me.calculateButton.Name = "calculateButton"
+        Me.calculateButton.Size = New System.Drawing.Size(75, 23)
+        Me.calculateButton.TabIndex = 0
+        Me.calculateButton.Text = "Calculate"
+        Me.calculateButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Amount"
+        '
+        'amountTextBox
+        '
+        Me.amountTextBox.Location = New System.Drawing.Point(95, 43)
+        Me.amountTextBox.Name = "amountTextBox"
+        Me.amountTextBox.Size = New System.Drawing.Size(161, 20)
+        Me.amountTextBox.TabIndex = 2
+        '
+        'ShowAmountInWords
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 152)
+        Me.Controls.Add(Me.amountTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.calculateButton)
+        Me.Name = "ShowAmountInWords"
+        Me.Text = "Show Amount in Words"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents calculateButton As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents amountTextBox As System.Windows.Forms.TextBox
+
+End Class
